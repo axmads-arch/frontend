e84bbbd9d8eee2c209e5f77fa53340dcd7f3d4f2
Axmad <ahmadjonabdullayev23@gmail.com>
2026-07-15 04:40:03 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Service Worker ro'yxatga olish
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered:', reg.scope))
+      .catch(err => console.log('SW error:', err));
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
